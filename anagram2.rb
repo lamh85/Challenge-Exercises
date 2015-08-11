71e66a68df6f3b06317d01c18fe28c57c4ec2691
Hon Lam <lamh85@gmail.com>
2015-08-10 19:07:06 -0700

    Started second attempt at the Anagram problem.

---
+++ anagram2.rb
@@ -0,0 +1,19 @@
+# This is my second attempt at the Anagram problem.
+
+puts "Give me a number!"
+
+input = gets.chomp
+
+puts "Your number is #{input}"
+
+# Convert to strong
+# Split each digit into an array of digits
+
+
+# EXAMPLES:
+# #########
+# Input: 3222
+# Answer: 3222
+
+# Input: 2321
+# Answer: 2### -> 22## -> 2231
