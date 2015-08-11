011d5a722e1097f336b32d6dcf0b2f174b1da26c
Hon Lam <lamh85@gmail.com>
2015-08-10 19:12:22 -0700

    Anagram: More examples to help planning.

--- anagram2.rb
+++ anagram2.rb
@@ -9,11 +9,20 @@ puts "Your number is #{input}"
 # Convert to strong
 # Split each digit into an array of digits
 
+=begin
 
-# EXAMPLES:
-# #########
-# Input: 3222
-# Answer: 3222
+EXAMPLES:
+Input: 3222
+Answer: 3222
 
-# Input: 2321
-# Answer: 2### -> 22## -> 2231
+Input: 2321
+Answer: 2bcd -> 22cd -> 2231
+
+Input: 23231
+Answer: 2bcde -> 23cde -> 232de -> 2321e -> 23213
+
+Reasoning:
+first digit) keep the first digit
+second digit) find the digit that is second largest or equal
+
+=end
