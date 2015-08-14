8d90ab55fecc12ca0932effa79cc801bece0ce68
Hon Lam <lamh85@gmail.com>
2015-08-14 00:20:36 -0700

    Poker cards: More validation logic.

--- poker-cards.rb
+++ poker-cards.rb
@@ -4,6 +4,7 @@ puts "To decode, use this format: [0, 51, 30, 22, 2]"
 @input = gets.chomp.to_s
 
 @is_invalid = false
+@validation_msg = ""
 
 def encode
   puts "I am encoding"
@@ -17,6 +18,14 @@ def check_elements(correct_class)
   @input.each do |element|
     if element.class != correct_class
       @is_invalid = true
+      @validation_msg = "Error: at least one of the elements' format does not match the others'."
+    end
+    if (correct_class == String) && ((element[0].to_i < 2 || element[0].to_i > 9) && (element[0].upcase != "A" || element[0].upcase != "T" || element[0].upcase != "J" || element[0].upcase != "Q" element[0].upcase != "K"))
+      @is_invalid = true
+      @validation_msg = "This array element does not represent a card value (A to K): #{element}."
+    elsif (correct_class == Fixnum) && (element < 0 || element > 51)
+      @is_invalid = true
+      @validation_msg = "This array element is not between 0 and 51 inclusive: #{element}."
     end
   end
 end
@@ -33,7 +42,11 @@ def validate
     @is_invalid = true
   end
   if @is_invalid == true
-    puts "Your input is not valid."
+    if @validation_msg == ""
+      puts "Your array did not pass a validation error"
+    else
+      puts @validation_msg
+    end
   elsif @input[0].class == Fixnum
     decode
   else
