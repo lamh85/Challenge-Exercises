24589af208bc7f6f60f1cb8bb99c5dd7487aedbc
Hon Lam <lamh85@gmail.com>
2015-08-13 23:53:14 -0700

    Poker cards: Validation starting to work.

--- poker-cards.rb
+++ poker-cards.rb
@@ -3,6 +3,8 @@ puts "To encode, use this format: ['Ac', 'Ks', '5h', 'Td', '3c']"
 puts "To decode, use this format: [0, 51, 30, 22, 2]"
 @input = gets.chomp.to_s
 
+@is_invalid = false
+
 def encode
   puts "I am encoding"
 end
@@ -11,19 +13,26 @@ def decode
   puts "I am decoding"
 end
 
-def validate
-  is_invalid = false
-  @input.each_with_index do |element,index|
-    if (@input[index].class != @input[index-1].class) || (@input[index].class != @input[index+1].class)
-      puts "==="
-      puts "The neutral element's class is #{@input[index].class}"
-      puts "The previous element's class is #{@input[index-1].class}"
-      puts "The next element's class is #{@input[index+1].class}"
-      is_invalid = true
-      break
+def check_elements(correct_class)
+  @input.each do |element|
+    if element.class != correct_class
+      @is_invalid = true
     end
   end
-  if is_invalid == true
+end
+
+def validate
+  if @input[0].class == String
+    puts "checkpoint 1"
+    check_elements(String)
+  elsif @input[0].class == Fixnum
+    puts "checkpoint 2"
+    check_elements(Fixnum)
+  else
+    puts "checkpoint 3"
+    @is_invalid = true
+  end
+  if @is_invalid == true
     puts "Your input is not valid."
   elsif @input[0].class == Fixnum
     decode
@@ -32,15 +41,30 @@ def validate
   end
 end
 
-def to_array
-  if @input.class != Array
-    ["[", "]", " "].each do |replace_me|
-      @input.gsub!(replace_me,"")
-    end
-    @input = @input.split(",")
+def make_array
+  puts "I am a #{@input.class}"
+  puts "I am a #{@input[0]}"
+  ["[", "]", " "].each do |replace_me|
+    @input.gsub!(replace_me,"")
   end
-  puts "The value of to_array is #{to_array}"
+  @input = @input.split(",")
+  puts "The array is now #{@input}"
+  # If a string can stay the same after to_i.to_s, then it is a number
+  if @input[0].to_i.to_s == @input[0]
+    @input.each_with_index do |element,index|
+      # puts "The element is #{element}"
+      # puts "The index is #{index}"
+      @input[index] = @input[index].to_i
+      puts "The element's class is now #{@input[index].class}"
+    end
+  puts "The array is now #{@input}"
   validate
+  end
+
+  # puts "The value of make_array is #{@input}"
+  # validate
 end
 
-to_array
+make_array
+# puts @input.class
+# puts @input
