0fb6e32fa491fc4b2a78788be7e9a85d86182f80
Hon Lam <lamh85@gmail.com>
2015-08-17 17:46:22 -0700

    Sorted List: Added methods for viewing and adding.

--- sorted-list.rb
+++ sorted-list.rb
@@ -1,11 +1,25 @@
+# To run this app: irb -r ./sorted-list.rb
+
 class List
 
-  def say
-    puts "hello world"
+  @array = []
+
+  def self.all
+    puts "The list is now: #{@array}"
+  end
+
+  def self.length
+    puts "The size of the list is #{@array.length}"
+  end
+
+  def self.add(new_item)
+    @array << new_item
+    puts "Added this element: #{new_item}"
+    puts "The list is now: #{@array}"
+  end
+
+  def self.get(position)
+    puts "The element is #{@array[position-1]}"
   end
 
 end
-
-foo = List.new
-
-foo.say
