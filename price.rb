085188c6169c1a8d49c829ebfb3b94086759211d
Hon Lam <lamh85@gmail.com>
2015-08-21 22:28:29 -0700

    Price: Started logic on the commas.

--- price.rb
+++ price.rb
@@ -2,4 +2,22 @@ puts "Give me a number!"
 
 input_array = gets.chomp.to_f.round(2).to_s.split(".")
 
-print input_array
+input_hash = {
+  whole: input_array[0],
+  decimals: input_array[1]
+}
+
+whole_reversed = input_hash[:whole].reverse.split("")
+
+whole_reversed_commas = []
+whole_reversed.each_with_index do |element,index|
+  whole_reversed_commas << element
+  if (index + 1) % 3 == 0
+    whole_reversed_commas << ","
+  end
+end
+
+whole_commas = whole_reversed_commas.reverse.join("")
+
+print whole_commas
+
