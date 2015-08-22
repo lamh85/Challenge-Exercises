4c0032802b7d9fb667c7e190c7803cfb86ba52f5
Hon Lam <lamh85@gmail.com>
2015-08-21 22:36:36 -0700

    Price: Finished logic for formatting. Next step: validation.

--- price.rb
+++ price.rb
@@ -12,12 +12,11 @@ whole_reversed = input_hash[:whole].reverse.split("")
 whole_reversed_commas = []
 whole_reversed.each_with_index do |element,index|
   whole_reversed_commas << element
-  if (index + 1) % 3 == 0
+  if ((index + 1) % 3 == 0) && (index + 1 != whole_reversed.length)
     whole_reversed_commas << ","
   end
 end
 
-whole_commas = whole_reversed_commas.reverse.join("")
+final_string = "$" + whole_reversed_commas.reverse.join("") + "." + input_hash[:decimals]
 
-print whole_commas
-
+puts "Your number in a price format is #{final_string}."
