2eb9d7f168942e9ec3d84e0dc976950f1eba716a
Hon Lam <lamh85@gmail.com>
2016-05-03 23:51:44 -0700

    Started new Ruby file.

---
+++ spreadsheet_totals.rb
@@ -0,0 +1,28 @@
+hash = {
+  cogs: {
+    jan: 100,
+    feb: 200,
+    mar: 300,
+  },
+  utilities: {
+    jan: 400,
+    feb: 500,
+    mar: 600
+  },
+  shipping: {
+    jan: 700,
+    feb: 800,
+    mar: 900
+  },
+  total: {
+    jan: nil,
+    feb: nil,
+    mar: nil
+  }
+}
+
+hash.each do |key, value|
+  hash[key].each do |deep_key, deep_value|
+    puts deep_value
+  end
+end
